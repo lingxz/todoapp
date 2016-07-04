8a4eb845cfea133a1f198b8107d40c37a6834c7d
Lingyi Hu <hulingyi1995@yahoo.com.sg>
2016-07-04 20:09:59 +0100

    changed angular binding tags

--- static/script.js
+++ static/script.js
@@ -1,23 +1,19 @@
 var app = angular.module("TodoApp", []);
-//
-// app.config(function ($routeProvider) {
-//     $routeProvider
-//     // TODO: not sure why this doesn't work
-//     // route for home page
-//         .when('/asd', {
-//             templateUrl: '../static/todo.html',
-//             controller: 'mainController'
-//         });
-//     // .otherwise({ redirectTo: '/'})
-// });
+
+app.config(['$interpolateProvider', function($interpolateProvider) {
+    $interpolateProvider.startSymbol('{[');
+    $interpolateProvider.endSymbol(']}');
+}]);
 
 app.controller("mainController", [
     '$scope',
     '$http',
     function ($scope, $http) {
-        $scope.state = {};
+        $scope.tasks = [{content:"asdf"}, {content:"hello"}];
+        $scope.newtask = "";
         $scope.addTask = function () {
-            console.log("posted" + $scope.newtask);
+            $scope.tasks.unshift({content:$scope.newtask});
+            $scope.newtask = ""
         }
     }
 ]);
