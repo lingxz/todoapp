305ee696c0c222fca507d24b17c2ed46343c78d5
Mark Lee <markleechuyong@gmail.com>
2016-08-04 08:02:11 +0800

    Modified karma test running to work on travis

--- karma.conf.js
+++ karma.conf.js
@@ -1,97 +1,108 @@
 // Karma configuration
 // Generated on Sun Jul 31 2016 19:04:34 GMT+0100 (GMT Summer Time)
 
-module.exports = function(config) {
-  config.set({
+module.exports = function (config) {
+    var configuration = {
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: '',
 
 
-    // list of files / patterns to load in the browser
-    files: [
-      'node_modules/jquery/dist/jquery.js',
-      'node_modules/angular/angular.js',
-      'node_modules/bootstrap/dist/js/bootstrap.js',
-      'node_modules/angular-mocks/angular-mocks.js',
-      'node_modules/angular-animate/angular-animate.js',
-      'node_modules/angular-aria/angular-aria.js',
-      'node_modules/angular-elastic/elastic.js',
-      'node_modules/angular-hotkeys/build/hotkeys.js',
-      'node_modules/angular-route/angular-route.js',
-      'node_modules/angular-ui-bootstrap/dist/ui-bootstrap-tpls.js',
-      'node_modules/angular-ui-bootstrap-datetimepicker/datetimepicker.js',
-      'project/**/*.js',
-      'project/**/**/*.js',
-      'project/static/*.html',
-      'project/static/**/*.html'
-    ],
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['jasmine'],
 
 
-    // list of files to exclude
-    exclude: [
-    ],
+        // list of files / patterns to load in the browser
+        files: [
+            'node_modules/jquery/dist/jquery.js',
+            'node_modules/angular/angular.js',
+            'node_modules/bootstrap/dist/js/bootstrap.js',
+            'node_modules/angular-mocks/angular-mocks.js',
+            'node_modules/angular-animate/angular-animate.js',
+            'node_modules/angular-aria/angular-aria.js',
+            'node_modules/angular-elastic/elastic.js',
+            'node_modules/angular-hotkeys/build/hotkeys.js',
+            'node_modules/angular-route/angular-route.js',
+            'node_modules/angular-ui-bootstrap/dist/ui-bootstrap-tpls.js',
+            'node_modules/angular-ui-bootstrap-datetimepicker/datetimepicker.js',
+            'project/**/*.js',
+            'project/**/**/*.js',
+            'project/static/*.html',
+            'project/static/**/*.html'
+        ],
 
 
+        // list of files to exclude
+        exclude: [],
 
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-      'project/static/*.html': ['ng-html2js'],
-      'project/static/**/*.html': ['ng-html2js'],
-      'project/static/*.js': ['coverage'],
-      'project/static/**/*.js': ['coverage']
-    },
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {
+            'project/static/*.html': ['ng-html2js'],
+            'project/static/**/*.html': ['ng-html2js'],
+            'project/static/*.js': ['coverage'],
+            'project/static/**/*.js': ['coverage']
+        },
 
-    ngHtml2JsPreprocessor: {
-      // // strip this from the file path
-      stripPrefix: 'project/',
-      moduleName: 'templates'
-    },
-
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage'],
-
-    coverageReporter: {
-      type : 'html',
-      // output coverage reports
-      dir : 'coverage/'
-    },
+        ngHtml2JsPreprocessor: {
+            // // strip this from the file path
+            stripPrefix: 'project/',
+            moduleName: 'templates'
+        },
 
 
-    // web server port
-    port: 9876,
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['progress', 'coverage'],
+
+        coverageReporter: {
+            type: 'html',
+            // output coverage reports
+            dir: 'coverage/'
+        },
 
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+        // web server port
+        port: 9876,
 
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: false,
 
 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true
-  })
-}
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: ['Chrome'],
+
+
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: true,
+
+        // For Travis CI
+        customLaunchers: {
+            Chrome_travis_ci: {
+                base: 'Chrome',
+                flags: ['--no-sandbox']
+            }
+        }
+    };
+
+    if (process.env.TRAVIS) {
+        configuration.browsers = ['Chrome_travis_ci'];
+    }
+
+    config.set(configuration);
+};
