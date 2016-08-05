9ab54b5d40b66b930c58ff0071a9ab101b2b6c82
lingxz <hulingyi1995@yahoo.com.sg>
2016-08-06 00:28:36 +0100

    tests for login, logout, register controllers
    no idea why these two tests are failing

---
+++ project/tests/unit/logoutController.spec.js
@@ -0,0 +1,74 @@
+describe('logoutController', function () {
+
+    var scope,
+        $rootScope,
+        $controller,
+        $q,
+        $location,
+        AuthService,
+        AUTH_EVENTS,
+        deferred,
+        controller,
+        createController;
+
+    beforeEach(module('templates'));
+    beforeEach(module('todoApp'));
+
+    beforeEach(inject(function (_$controller_, _$rootScope_, _$q_, _$location_, _AuthService_, _AUTH_EVENTS_) {
+        $controller = _$controller_;
+        $rootScope = _$rootScope_;
+        $q = _$q_;
+        $location = _$location_;
+        AuthService = _AuthService_;
+        AUTH_EVENTS = _AUTH_EVENTS_;
+        scope = $rootScope.$new();
+        createController = function () {
+            return $controller('logoutController', {
+                '$scope': scope
+            })
+        };
+
+        controller = createController();
+        deferred = $q.defer();
+        spyOn(AuthService, 'logout').and.returnValue(deferred.promise);
+    }));
+
+    describe('logout function', function () {
+
+        it('should exist', function () {
+            expect(angular.isFunction(scope.logout)).toBe(true)
+        });
+
+        it('should call logout function from AuthService', function () {
+            scope.logout();
+            expect(AuthService.logout).toHaveBeenCalled()
+        });
+
+        it('should broadcast logoutSuccess event on successful logout', function () {
+            spyOn($rootScope, '$broadcast').and.callThrough();
+            scope.logout();
+            deferred.resolve();
+            scope.$digest();
+            expect($rootScope.$broadcast).toHaveBeenCalledWith(AUTH_EVENTS.logoutSuccess)
+        });
+
+        it('should redirect to login page on successful logout', function () {
+            spyOn($location, 'path').and.callThrough();
+            scope.logout();
+            deferred.resolve();
+            scope.$digest();
+
+            expect($location.path).toHaveBeenCalledWith('/login');
+        });
+
+        it('should not redirect or do anything on unsuccessful logout', function () {
+            spyOn($location, 'path').and.callThrough();
+            spyOn($rootScope, '$broadcast').and.callThrough();
+            scope.logout();
+            deferred.reject();
+            scope.$digest();
+            expect($location.path).not.toHaveBeenCalled();
+            expect($rootScope.$broadcast).not.toHaveBeenCalled();
+        })
+    })
+});
